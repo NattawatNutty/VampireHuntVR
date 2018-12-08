5cc9b38e1028ea89122dc2329f6b7e4a13cdb047
mathawee31 <44827425+mathawee31@users.noreply.github.com>
2018-12-08 17:55:48 +0700

    Test NavMesh
    Object can walk from starting point to destination but it cannot connect with the player in VR

---
+++ Assets/Scenes/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour {
+
+   //public Camera cam;
+   // public NavMeshAgent agent;
+
+
+
+    //void Update() {
+    //    if (Input.GetMouseButtonDown(0)) {
+    //        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+    //        RaycastHit hit;
+    //        if (Physics.Raycast(ray, out hit)) {
+    //            move our agent
+    //            agent.SetDestination(hit.point);
+    //        }
+
+    //    }
+
+    //    if (Input.GetMouseButtonDown(0)) {
+    //        agent.SetDestination(Input.mousePosition);
+    //    }
+    //}
+
+
+
+    // NavMesh - set destination object
+    [SerializeField]
+    Transform des;
+    NavMeshAgent navMeshAgent;
+
+    //  Use this for initialization
+
+    void Start() {
+
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if (navMeshAgent == null) {
+            Debug.Log("Error " + gameObject.name);
+        } else {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination() {
+        if (des != null) {
+            Vector3 targetVector = des.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+
+    }
+
+}
